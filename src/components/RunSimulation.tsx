997e9c38873c42a4e460f7fcdc830cc69acb864c
Fenya96 <Fenjvesi.Akos@stud.u-szeged.hu>
2024-01-24 20:26:53 +0100

    add page for simulations

---
+++ src/components/RunSimulation.tsx
@@ -0,0 +1,23 @@
+import { Box, Step, StepLabel, Stepper } from "@mui/material";
+
+const steps = [
+    'Settings',
+    'Simulation parameters'
+]
+
+function RunSimulation() {
+  return (
+    <Box sx={{ width: '100%'}}>
+        <Stepper activeStep={1} alternativeLabel>
+            {steps.map((label) => (
+                <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                </Step>
+            ))}
+        </Stepper>
+
+    </Box>
+  )
+}
+
+export default RunSimulation

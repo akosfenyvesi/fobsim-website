af8182033ee2bc24b72760710e0259878634bcbb
Fenya96 <Fenjvesi.Akos@stud.u-szeged.hu>
2024-01-24 20:29:37 +0100

    add home to routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import RunSimulation from './RunSimulation';
+import Home from './Home';
 
 function AppRouter() {
   return (
     <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/simulation" element={<RunSimulation />} />

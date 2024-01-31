c467647224170b0947ba9aa036302f6c2011f58f
Fenya96 <Fenjvesi.Akos@stud.u-szeged.hu>
2024-01-31 21:53:42 +0100

    remove Divider

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -4,7 +4,6 @@ import {
   Checkbox,
   Container,
   CssBaseline,
-  Divider,
   FormControlLabel,
   Grid,
   LinearProgress,
@@ -64,71 +63,73 @@ function SignIn() {
       <Typography component="h1" variant="h5">
         Log In
       </Typography>
-      <Box sx={{ mt: 1 }}>
-        <Button
-          onClick={loginWithGoogle}
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3 }}
-        >
-          Login With Google
-        </Button>
-      </Box>
-      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-        <TextField
-          value={user.email}
-          onChange={handleChange}
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          value={user.password}
-          onChange={handleChange}
-          margin="normal"
-          required
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          autoComplete="new-password"
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          disabled={loading}
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3 }}
-        >
-          Log In
-        </Button>
-        {loading && <LinearProgress />}
-        <Grid container>
-          <Grid item xs>
-            <Link
-              component={ReactRouterLink}
-              to="/forgot-password"
-              variant="body2"
-            >
-              Forgot password?
-            </Link>
+      <Box>
+        <Box sx={{ mt: 1 }}>
+          <Button
+            onClick={loginWithGoogle}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3 }}
+          >
+            Login With Google
+          </Button>
+        </Box>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            value={user.email}
+            onChange={handleChange}
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            value={user.password}
+            onChange={handleChange}
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="new-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            disabled={loading}
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3 }}
+          >
+            Log In
+          </Button>
+          {loading && <LinearProgress />}
+          <Grid container>
+            <Grid item xs>
+              <Link
+                component={ReactRouterLink}
+                to="/forgot-password"
+                variant="body2"
+              >
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link component={ReactRouterLink} to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Link component={ReactRouterLink} to="/signup" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
     </Container>
   );
